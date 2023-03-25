8e5201649f4a00973af8331ffd85df18b6456a12
Diegofr <120336092+Diegofrjz@users.noreply.github.com>
2023-03-25 13:20:17 -0500

    Update MP3DowloaderWithUI.py

--- MP3DowloaderWithUI.py
+++ MP3DowloaderWithUI.py
@@ -4,13 +4,13 @@ from tkinter import filedialog
 from pytube import YouTube
 
 def download_audio():
-    # Obtenemos la URL del video de YouTube ingresada por el usuario
+    # Obtenemos la URL del video de YouTube
     video_url = url_entry.get()
 
-    # Pedimos al usuario que seleccione la ubicación donde quiere guardar el archivo de audio
+    # Ubicación donde quiere guardar el archivo de audio
     audio_path = filedialog.asksaveasfilename(defaultextension='.mp3', filetypes=[('Archivo de audio', '*.mp3')])
 
-    # Descargamos el video y extraemos el audio en formato mp3
+    # Descargamos el video y extraemos el audio
     yt = YouTube(video_url)
     audio = yt.streams \
         .filter(only_audio=True, file_extension='mp4') \
@@ -28,17 +28,17 @@ def download_audio():
     else:
         status_label.config(text='No se encontró ningún audio en formato mp3 en el video.')
 
-# Creamos la ventana principal
+# Ventana principal
 root = Tk()
 root.title('Descargar audio de YouTube')
 
-# Creamos los widgets de la interfaz gráfica
+# Widgets de la interfaz gráfica
 url_label = Label(root, text='Ingresa la URL del video de YouTube:')
 url_entry = Entry(root, width=50)
 download_button = Button(root, text='Descargar audio', command=download_audio)
 status_label = Label(root, text='')
 
-# Acomodamos los widgets en la ventana principal
+# Acomodamos los widgets
 url_label.grid(row=0, column=0, padx=10, pady=10)
 url_entry.grid(row=0, column=1, padx=10, pady=10)
 download_button.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
